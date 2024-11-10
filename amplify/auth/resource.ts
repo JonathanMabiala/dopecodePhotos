f70879fb681872789f01b8fb03a99b5e36dc12a1
JonathanMabiala <62263731+JonathanMabiala@users.noreply.github.com>
2024-11-10 15:46:46 -0500

    google and facebook config

--- amplify/auth/resource.ts
+++ amplify/auth/resource.ts
@@ -1,4 +1,4 @@
-import { defineAuth } from '@aws-amplify/backend';
+import { defineAuth, secret } from "@aws-amplify/backend";
 
 /**
  * Define and configure your auth resource
@@ -7,5 +7,21 @@ import { defineAuth } from '@aws-amplify/backend';
 export const auth = defineAuth({
   loginWith: {
     email: true,
+    externalProviders: {
+      google: {
+        clientId: secret("GOOGLE_CLIENT_ID"),
+        clientSecret: secret("GOOGLE_CLIENT_SECRET"),
+      },
+      facebook: {
+        clientId: secret("FACEBOOK_CLIENT_ID"),
+        clientSecret: secret("FACEBOOK_CLIENT_SECRET"),
+      },
+
+      callbackUrls: [
+        "http://localhost:3000/profile",
+        "https://mywebsite.com/profile",
+      ],
+      logoutUrls: ["http://localhost:3000/", "https://mywebsite.com"],
+    },
   },
 });
